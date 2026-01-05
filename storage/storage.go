d3c6073630a6ca1f07e89a58a7dcdd75cce201e1
JGpGH <j.gauthier130@gmail.com>
2026-01-04 22:17:18 -0500

    using reference instead of copy

--- storage/storage.go
+++ storage/storage.go
@@ -45,7 +45,7 @@ func NewTrashable[T Indexable](value T, canBeTrashed bool) Trashable[T] {
 
 func NewTrashables[T Indexable](values []T, canBeTrashed bool) []Trashable[T] {
 	var result []Trashable[T]
-	for k, _ := range values {
+	for k := range values {
 		result = append(result, NewTrashable(values[k], canBeTrashed))
 	}
 	return result
