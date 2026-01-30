ca39a59a8cee75b0c070ca497cab5ee3393617f2
JGpGH <j.gauthier130@gmail.com>
2026-01-29 22:58:46 -0500

    re-export errors because

--- golfu.go
+++ golfu.go
@@ -4,8 +4,14 @@ import (
 	"context"
 
 	"github.com/JGpGH/golfu/element"
+	lfuErr "github.com/JGpGH/golfu/errors"
 	"github.com/JGpGH/golfu/internal"
 	"github.com/JGpGH/golfu/storage"
+)
+
+var (
+	ErrNotFound     = lfuErr.ErrNotFound
+	ErrInvalidIndex = lfuErr.ErrInvalidIndex
 )
 
 func NewCachedStorage[T element.Indexable](ctx context.Context, cold storage.ColdStorage[T], maxUnits int) storage.CachedStorage[T] {
